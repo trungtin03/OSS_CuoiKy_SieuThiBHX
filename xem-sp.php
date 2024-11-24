83d9fef517dfcbbb566a44825da4791dd98ffc7d
Lukari <lukari87@gmail.com>
2024-11-24 13:43:22 +0700

    update

--- xem-sp.php
+++ xem-sp.php
@@ -76,7 +76,7 @@
                 xhr.onreadystatechange = function() {
                     if (xhr.readyState === 4 && xhr.status === 200) {
                         location.reload();
-                        window.location.href = "trang-chu.php"; // Redirect to the homepage after successful logout
+                        window.location.href = "index.php"; // Redirect to the homepage after successful logout
                     }
                 };
 
@@ -163,7 +163,7 @@
                                     <ul class="dropdown-list-1">
                                         <li><a href="don-hang.php">Đơn hàng</a></li>
                                         <li><a href="tai-khoan.php">Thông tin tài khoản</a></li>
-                                        <li><a href="#" id="logout">Đăng xuất</a></li>
+                                        <li><a href="logout.php" id="logout">Đăng xuất</a></li>
                                     </ul>
                                 </li>
                                 <style>
